9e58d5420c7f7c54d34250f7da791378fe8f6e4c
sanSkiller <1290148953@qq.com>
2020-10-07 21:45:55 +0800

    updated:添加帖子添加编辑页面

--- web/src/router.js
+++ web/src/router.js
@@ -53,6 +53,10 @@ const NoFound = () =>
 const Confirm = () =>
   import(/* webpackChunkName: 'confirm' */ "@/views/Confirm.vue");
 const Reset = () => import(/* webpackChunkName: 'reset' */ "@/views/Reset.vue");
+const Add = () =>
+  import(/* webpackChunkName: 'add' */ "@/components/contents/Add.vue");
+const Edit = () =>
+  import(/* webpackChunkName: 'edit' */ "@/components/contents/Edit.vue");
 
 Vue.use(Router);
 
@@ -112,6 +116,43 @@ const router = new Router({
       name: "home",
       props: true,
       component: User
+    },
+
+    {
+      path: "/add",
+      name: "add",
+      component: Add,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/edit/:tid",
+      props: true,
+      name: "edit",
+      component: Edit,
+      meta: { requiresAuth: true },
+      beforeEnter(to, from, next) {
+        // 正常的情况 detail
+        if (
+          ["detail", "mypost"].indexOf(from.name) !== -1 &&
+          to.params.page &&
+          to.params.page.isEnd === "0"
+        ) {
+          next();
+        } else {
+          // 用户在edit页面刷新的情况
+          const editData = localStorage.getItem("editData");
+          if (editData && editData !== "") {
+            const editObj = JSON.parse(editData);
+            if (editObj.isEnd === "0") {
+              next();
+            } else {
+              next("/");
+            }
+          } else {
+            next("/");
+          }
+        }
+      }
     },
     {
       path: "/center",
