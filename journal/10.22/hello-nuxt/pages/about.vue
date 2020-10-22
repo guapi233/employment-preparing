97b679e21dd1a7cc1e664bbaccf28207af555149
sanSkiller <1290148953@qq.com>
2020-10-22 22:34:21 +0800

    updated:10.22

---
+++ journal/10.22/hello-nuxt/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    关于
+    <img src="../../../../../自考.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ journal/10.22/hello-nuxt/pages/list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    列表页: {{ name }}
+    <!-- {{ a }} -->
+    <!-- {{ num }} -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: "abbb",
+      // num: a,
+    };
+  },
+  async beforeCreate() {
+    let a = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(3);
+      }, 3000);
+    });
+    console.log(this.a, "??");
+  },
+  created() {
+    console.log(this.a, "!!");
+  },
+  async asyncData(context) {
+    return {
+      name: "崔永杰",
+      // num: a,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
